83fe4495629d1e236265be12f4e51d40441b8afa
Sikander <sikku.rk405@gmail.com>
2022-02-15 21:29:44 +0530

    [Sikander] Added bubble sort program

---
+++ AlgoPrograms/AlgoPrograms/Bubblesort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoPrograms
+{
+    internal class Bubblesort
+    {     
+           public void Sort()
+            {
+                int[] arr = { 6, 23, 47, 25, 73, 7 };
+                int temp;
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            temp = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = temp;
+                        }
+                    }
+                }
+                Console.WriteLine("Sorted array :");
+                foreach (int val in arr)
+                Console.WriteLine(val);
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ AlgoPrograms/AlgoPrograms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Enter 1 for Bubble Sort Problem");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Bubblesort sort = new Bubblesort();
+                    sort.Sort();
+                    Console.ReadKey();
+                    break;
+                    default:
+                    Console.WriteLine("Enter the correct option:");
+                    break;
+            }
+        }
+    }
+}
